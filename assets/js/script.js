0a898cea89d38f61f826d180fdee5d8587a1bea4
olavo argemiro <olaviochampions@gmail.com>
2021-05-09 18:42:37 -0300

    Meu primeiro cartaz de filmes

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $.ajax({
+        url:'https://alunos.b7web.com.br/cinema/',
+        type: 'GET',
+        dataType: 'json',
+        success:function(json){
+            console.log(json);
+        }
+    });
+});    
